96af14e527d6e8b46c92b0d65fd39e81c333597b
parth.p3 <parth.p@latitudetechnolabs.com>
2022-03-05 14:42:40 +0530

    Project created and basic structure added

---
+++ eddi_backend/eddi_app/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+from supplier.models import *
+
+# Create your models here.
---
+++ eddi_backend/eddi_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from supplier.urls import *
+
+urlpatterns = [
+    # path('',view_here)
+]
