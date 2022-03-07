db1c17a0cfa3dd693d62051db5f7b11302ece09a
parth.p3 <parth.p@latitudetechnolabs.com>
2022-03-07 19:55:12 +0530

    User and course api done

--- eddi_app/urls.py
+++ eddi_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('changepassword/<str:uuid>/',ChangePasswordView.as_view()),
     path('get-user-details/<str:uuid>/',GetUserDetails.as_view()),
     path('get-user-details/',GetUserDetails.as_view()),
-    path('add-course/',AddCourseView.as_view()),
+    path('add-course/',AddCourseView.as_view()), 
     path('get-course-details/<str:uuid>/',GetCourseDetails.as_view()),
     path('get-course-details/',GetCourseDetails.as_view()),
 
