e337afeec38a51f8ba44660cd0b4689215959daf
nishant.k1 <nishant.k@latitudetechnolabs.com>
2022-03-19 17:34:20 +0530

    nishant update

--- eddi_backend/settings.py
+++ eddi_backend/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-SECURE_SSL_REDIRECT = True
 # Application definition
 
 INSTALLED_APPS = [
@@ -46,7 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_swagger'
 ]
-
+SECURE_SSL_REDIRECT = True
 JAZZMIN_SETTINGS = {
     # title of the window (Will default to current_admin_site.site_title if absent or None)
     "site_title": "Eddi Admin",
@@ -161,10 +160,21 @@ CKEDITOR_CONFIGS = {
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Eddi_db',
+        'USER': 'Eddi_db',
+        'PASSWORD': 'W0rxQxlWsKuCekrg',
+        'HOST': 'eddi-backend.testyourapp.online',
+        'PORT': '3306',
     }
 }
 
