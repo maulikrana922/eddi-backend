798feed97e925f92538a6e2715c05fba224eb6b1
nishant.k1 <nishant.k@latitudetechnolabs.com>
2022-03-19 19:16:18 +0530

    nishant update

--- eddi_backend/settings.py
+++ eddi_backend/settings.py
@@ -32,9 +32,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['*','eddi-backend.testyourapp.online']
 
 # Application definition
-
 INSTALLED_APPS = [
-     'jazzmin',
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
