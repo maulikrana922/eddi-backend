8f15862c5eda1a8825d49656ebd09a086bbbfa33
parth.p3 <parth.p@latitudetechnolabs.com>
2022-04-04 18:22:03 +0530

    Update course detail logic

--- eddi_app/models.py
+++ eddi_app/models.py
@@ -108,7 +108,8 @@ def send_appointment_confirmation_email(sender, instance, created, **kwargs):
 
         html_path = OTP_EMAIL_HTML
         otp = PasswordView()
-        context_data = {'final_otp':otp}
+        fullname = f'{instance.first_name} {instance.last_name}'
+        context_data = {'final_otp':otp,'fullname':fullname}
         email_html_template = get_template(html_path).render(context_data)
         email_from = settings.EMAIL_HOST_USER
         recipient_list = (instance.email_id,)
