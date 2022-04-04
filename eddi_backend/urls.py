2562c1192b202f4a855d163792f6e585658a452c
parth.p3 <parth.p@latitudetechnolabs.com>
2022-04-04 09:56:50 +0530

    Update

--- eddi_backend/urls.py
+++ eddi_backend/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path('docs/7e3f7124d67d0b6471842d311388b818/', TemplateView.as_view(
         template_name='documentation.html',
         extra_context={'schema_url':'openapi-schema'}
-    ), name='swagger-ui'),
+    ), name ='swagger-ui'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns +=staticfiles_urlpatterns()
