87f5fbe8da5007c00393e4ce13de76a0d1b77115
parth.p3 <parth.p@latitudetechnolabs.com>
2022-04-04 10:05:59 +0530

    Update

--- eddi_backend/settings.py
+++ eddi_backend/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     'rest_framework_swagger',
     'corsheaders'
 ]
-# LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
+# LOCALE_PATHS = [os. path.join(BASE_DIR, 'locale')]
 JAZZMIN_SETTINGS = {
     "language_chooser": True,
     # title of the window (Will default to current_admin_site.site_title if absent or None)
