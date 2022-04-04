947a5b073d0d98a08cec345ea09c12b793788378
nishant.k1 <nishant.k@latitudetechnolabs.com>
2022-04-04 09:48:20 +0530

    nishant updated

--- eddi_backend/settings.py
+++ eddi_backend/settings.py
@@ -36,8 +36,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*','eddi-backend.testyourapp.online'] 
 
-CORS_ALLOWED_ORIGINS = [
-    'eddi-backend.testyourapp.online',
+CORS_ALLOWED_ORIGINS = ['*',
+    'eddi-backend.testyourapp.online'
 ]
 CSRF_TRUSTED_ORIGINS=['https://eddi-backend.testyourapp.online'] 
 
