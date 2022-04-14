70ca5d54e24563cd7616362ffbf537b717e4a208
nishant.k1 <nishant.k@latitudetechnolabs.com>
2022-04-14 23:41:24 +0530

    updated

--- eddi_app/supplier_views.py
+++ eddi_app/supplier_views.py
@@ -1,5 +1,6 @@
 from calendar import TUESDAY
 from typing import final
+from wsgiref.handlers import read_environ
 from pytz import timezone
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -84,6 +85,7 @@ class AddCourseView(APIView):
             ADDITIONAL_INFORMATION: request.POST.get(ADDITIONAL_INFORMATION,None),
             ORGANIZATION_LOCATION: request.POST.get(ORGANIZATION_LOCATION,None),
             SUB_AREA:request.POST.get(SUB_AREA,None),
+            "approval_status_id" : 2,
             STATUS_ID:1
         }
         record_map[CREATED_AT] = make_aware(datetime.datetime.now())
@@ -150,39 +152,81 @@ class GetSubCategoryDetails(APIView):
                 user_type_data = None
             if user_type_data:
                 if user_type_data == ADMIN_S:
-                    data = getattr(models,COURSE_SUBCATEGORY_TABLE).objects.all()
+                    data = getattr(models,COURSE_SUBCATEGORY_TABLE).objects.all().order_by("-created_date_time")
                 else:
-                    data = getattr(models,COURSE_SUBCATEGORY_TABLE).objects.filter(**{'supplier__email_id':email_id,STATUS_ID:1})
+                    data = getattr(models,COURSE_SUBCATEGORY_TABLE).objects.filter(**{'supplier__email_id':email_id,STATUS_ID:1}).order_by("-created_date_time")
             if serializer := SubCategoryDetailsSerializer(data, many=True):
                 return Response({STATUS: SUCCESS, DATA: serializer.data}, status=status.HTTP_200_OK)
             else:
                 return Response({STATUS: ERROR, DATA: serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self,request,uuid = None):
+        email_id =  get_user_email_by_token(request)
+        try:
+            user_type_data = getattr(models,USERSIGNUP_TABLE).objects.get(**{EMAIL_ID:email_id}).user_type.user_type
+        except Exception:
+            user_type_data = None
         if not uuid:
             return Response({STATUS: ERROR, DATA: "Not Able to get data"}, status=status.HTTP_400_BAD_REQUEST)
+
         try:
-            data = getattr(models,COURSE_SUBCATEGORY_TABLE).objects.get(**{UUID:uuid,STATUS:1})
-        except:
+            data = getattr(models,COURSE_SUBCATEGORY_TABLE).objects.get(**{UUID:uuid})
+            # data = getattr(models,COURSE_SUBCATEGORY_TABLE).objects.get(**{UUID:uuid,STATUS:1})
+        except Exception as ex:
+            print(ex, "ex")
             return Response({STATUS: ERROR, DATA: "Data Not Found"}, status=status.HTTP_400_BAD_REQUEST)
         
         try:
-            dataa = getattr(models,COURSE_CATEGORY_TABLE).objects.get(**{CATEGORY_NAME:request.POST.get(CATEGORY_NAME_ID)})
+            ''' old code '''
+            # dataa = getattr(models,COURSE_CATEGORY_TABLE).objects.get(**{CATEGORY_NAME:request.POST.get(CATEGORY_NAME_ID)})
+            '''new code'''
+            dataa = getattr(models,COURSE_CATEGORY_TABLE).objects.get(**{CATEGORY_NAME:data.category_name.category_name})
             print(dataa.id)
         except Exception as ex:
-            print(ex)
-
+            print(ex, "exxx")
             return Response({STATUS: ERROR, DATA: "Category Error"}, status=status.HTTP_400_BAD_REQUEST)
         record_map = {
 
             'category_name_id': dataa.id,
             SUBCATEGORY_NAME: request.POST.get(SUBCATEGORY_NAME,data.subcategory_name),
             SUBCATEGORY_IMAGE : request.FILES.get(SUBCATEGORY_IMAGE,data.subcategory_image),
-            STATUS_ID:1
+            # STATUS_ID:1
         }
+
+
+        if user_type_data:
+            if user_type_data == ADMIN_S:
+                if request.POST.get("status"):
+                    if request.POST.get("status") == "Active":
+                        record_map[STATUS_ID] = 1
+                    else:
+                        record_map[STATUS_ID] = 2
+                else:
+                    record_map["status"] = data.status
+                    
+                if request.POST.get("approval_status"):
+                    if request.POST.get("approval_status") == "Approved":
+                        record_map["is_approved_id"] = 1
+                    if request.POST.get("approval_status") == "Pending":
+                        record_map["is_approved_id"] = 2
+                    if request.POST.get("approval_status") == "Rejected":
+                        record_map["is_approved_id"] = 3
+                else:
+                    record_map["is_approved"] = data.is_approved
+
+            elif user_type_data == SUPPLIER_S:
+                if request.POST.get("status"):
+                    if request.POST.get("status") == "Active":
+                        record_map[STATUS_ID] = 1
+                    else:
+                        record_map[STATUS_ID] = 2
+                else:
+                    record_map["status"] = data.status
+
         record_map[MODIFIED_AT] = make_aware(datetime.datetime.now())
         record_map[MODIFIED_BY] = 'admin'
         record_map[UUID] = uuid4()
+        print(record_map, "recorddddd")
         for key,value in record_map.items():
             setattr(data,key,value)
         data.save()
@@ -209,7 +253,7 @@ class GetSubCategoryDetails(APIView):
         data.save()
         return Response({STATUS: SUCCESS, DATA: "Data Succesfully Deleted"}, status=status.HTTP_200_OK)
 
-@permission_classes([AllowAny])
+# @permission_classes([AllowAny])
 class GetCourseDetails(APIView):
     res = None
     domain_data = None
@@ -217,6 +261,7 @@ class GetCourseDetails(APIView):
 
     fav_data = None
     fav_dataa = None
+    
     def get(self, request,uuid = None):
         res = None
         fav_dataa = None
@@ -262,11 +307,20 @@ class GetCourseDetails(APIView):
                         print(i.course_name, "iiiiiiiiiiiiii")
                         total_individuals = getattr(models,COURSE_ENROLL_TABLE).objects.filter(**{"payment_detail__course_name":i.course_name}).count()
                         print(total_individuals, "totallllllllllll")
+                elif getattr(models,USERSIGNUP_TABLE).objects.get(**{EMAIL_ID:email_id}).user_type.user_type == ADMIN_S:
+                    print("adddddddddddddddddddddddddddddddddddddddddddddddddddddddddd")
+                    data_s = getattr(models,COURSEDETAILS_TABLE).objects.all()
+
+
                 else:
-                    cat = getattr(models,USER_PROFILE_TABLE).objects.get(**{EMAIL_ID:email_id})
-                    print(cat, "cattttttttttttttt")
-                    a = cat.course_category.split(",")
-                    print(a, "listtttttt")
+                    try:
+                        cat = getattr(models,USER_PROFILE_TABLE).objects.get(**{EMAIL_ID:email_id})
+                        print(cat, "cattttttttttttttt")
+                        a = cat.course_category.split(",")
+                    except Exception as ex:
+                        print(ex, "exxxxxxxxx")
+
+                    # print(a, "listtttttt")
                     # print("555555555555555")
                     organization_domain = email_id.split('@')[1]
                     data_s = getattr(models,COURSEDETAILS_TABLE).objects.filter(**{STATUS:1}).filter(Q(organization_domain = organization_domain) | Q(course_for_organization = False))
@@ -300,30 +354,45 @@ class GetCourseDetails(APIView):
        
 
     def put(self,request,uuid = None):
+        email_id =  get_user_email_by_token(request)
+        try:
+            user_type_data = getattr(models,USERSIGNUP_TABLE).objects.get(**{EMAIL_ID:email_id}).user_type.user_type
+        except Exception:
+            user_type_data = None
+            
         res = None
         if not uuid:
             return Response({STATUS: ERROR, DATA: "Not Able to get data"}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            data = getattr(models,COURSEDETAILS_TABLE).objects.get(**{UUID:uuid})
+        except Exception as ex:
+            print(ex, "exxxxx")
+            return Response({STATUS: ERROR, DATA: "Not Able to get data"}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
-            category_id = getattr(models,COURSE_CATEGORY_TABLE).objects.only(ID).get(**{CATEGORY_NAME:request.POST.get(COURSE_CATEGORY_ID,None)})
-            sub_category_id = getattr(models,COURSE_SUBCATEGORY_TABLE).objects.only(ID).get(**{SUBCATEGORY_NAME:request.POST.get(SUBCATEGORY_NAME_ID,None)})
-            course_type_id = getattr(models,COURSE_TYPE_TABLE).objects.only(ID).get(**{TYPE_NAME:request.POST.get(COURSE_TYPE_ID,None)})
-            fee_type_id = getattr(models,FEE_TYPE_TABLE).objects.only(ID).get(**{FEE_TYPE_NAME :request.POST.get(FEE_TYPE_ID,None)})
-            course_level_id = getattr(models,COURSE_LEVEL_TABLE).objects.only(ID).get(**{LEVEL_NAME : request.POST.get(COURSE_LEVEL_ID,None)})
+            category_id = getattr(models,COURSE_CATEGORY_TABLE).objects.only(ID).get(**{CATEGORY_NAME:data.course_category.category_name})
+            sub_category_id = getattr(models,COURSE_SUBCATEGORY_TABLE).objects.only(ID).get(**{SUBCATEGORY_NAME:data.course_subcategory.subcategory_name})
+            course_type_id = getattr(models,COURSE_TYPE_TABLE).objects.only(ID).get(**{TYPE_NAME:data.course_type.type_name})
+            fee_type_id = getattr(models,FEE_TYPE_TABLE).objects.only(ID).get(**{FEE_TYPE_NAME :data.fee_type.fee_type_name})
+            course_level_id = getattr(models,COURSE_LEVEL_TABLE).objects.only(ID).get(**{LEVEL_NAME : data.course_level.level_name})
         except Exception as ex:
+            print(ex, "ex")
             return Response({STATUS:ERROR, DATA: "Error Getting Data"}, status=status.HTTP_400_BAD_REQUEST)
-        try:
-            data = getattr(models,COURSEDETAILS_TABLE).objects.get(**{UUID:uuid,STATUS:1})
-        except Exception:
-            return Response({STATUS: ERROR, DATA: "Data Not Found"}, status=status.HTTP_400_BAD_REQUEST)
+        # try:
+        #     data = getattr(models,COURSEDETAILS_TABLE).objects.get(**{UUID:uuid,STATUS:1})
+        # except Exception:
+        #     print(ex, "exx")
+
+        #     return Response({STATUS: ERROR, DATA: "Data Not Found"}, status=status.HTTP_400_BAD_REQUEST)
 
         if request.POST.get(COURSE_FOR_ORGANIZATION) == 'true':
 
             test_str = data.supplier.email_id
             res = test_str.split('@')[1]
             print(res)
-
-        record_map = {
+        print("ooooooooooooooooooo")
+        try:
+            record_map = {
             COURSE_IMAGE: request.FILES.get(COURSE_IMAGE,data.course_image),
             COURSE_NAME: request.POST.get(COURSE_NAME,data.course_name),
             COURSE_LEVEL_ID : course_level_id.id,
@@ -331,22 +400,67 @@ class GetCourseDetails(APIView):
             COURSE_CATEGORY_ID : category_id.id,
             COURSE_SUBCATEGORY_ID: sub_category_id.id,
             COURSE_TYPE_ID : course_type_id.id,
-            COURSE_FOR_ORGANIZATION:eval(request.POST.get(COURSE_FOR_ORGANIZATION).title()),
+            # COURSE_FOR_ORGANIZATION:eval(request.POST.get(COURSE_FOR_ORGANIZATION).title()),
             COURSE_LANGUAGE:request.POST.get(COURSE_LANGUAGE),
             COURSE_CHECKOUT_LINK: request.POST.get(COURSE_CHECKOUT_LINK,None),
             ORGANIZATION_DOMAIN:res,
             FEE_TYPE_ID: fee_type_id.id,
             COURSE_PRICE: request.POST.get(COURSE_PRICE,data.course_price),
             ADDITIONAL_INFORMATION: request.POST.get(ADDITIONAL_INFORMATION,data.additional_information),
-            STATUS_ID:1,
+            # STATUS_ID:1,
         }
-        record_map[MODIFIED_AT] = make_aware(datetime.datetime.now())
-        record_map[MODIFIED_BY] = 'admin'
-        record_map[UUID] = uuid4()
-        for key,value in record_map.items():
-            setattr(data,key,value)
-        data.save()
-        return Response({STATUS: SUCCESS, DATA: "Data Succesfully Edited"}, status=status.HTTP_200_OK)
+            if user_type_data:
+                if user_type_data == ADMIN_S:
+                    if request.POST.get("status"):
+                        if request.POST.get("status") == "Active":
+                            record_map[STATUS_ID] = 1
+                        else:
+                            record_map[STATUS_ID] = 2
+                    else:
+                        record_map["status"] = data.status
+                        
+                    if request.POST.get("approval_status"):
+                        print("apppprovedd")
+                        if request.POST.get("approval_status") == "Approved":
+                            record_map["is_approved_id"] = 1
+                        if request.POST.get("approval_status") == "Pending":
+                            record_map["is_approved_id"] = 2
+                        if request.POST.get("approval_status") == "Rejected":
+                            record_map["is_approved_id"] = 3
+                    else:
+                        record_map["is_approved"] = data.is_approved
+
+                elif user_type_data == SUPPLIER_S:
+                    if request.POST.get("status"):
+                        if request.POST.get("status") == "Active":
+                            record_map[STATUS_ID] = 1
+                        else:
+                            try:
+                                data1 = getattr(models,COURSE_ENROLL_TABLE).objects.get(**{"payment_detail__course_name":data.course_name})
+                            except Exception as ex:
+                                print(ex, "exxxx")
+                                data1 = None
+                            if data1 is not None:
+                                return Response({STATUS: ERROR, DATA: "Someone Already Enrolled in This Course"}, status=status.HTTP_400_BAD_REQUEST)
+                            else:
+                                record_map[STATUS_ID] = 2
+
+                    else:
+                        record_map["status"] = data.status
+
+
+                record_map[MODIFIED_AT] = make_aware(datetime.datetime.now())
+                record_map[MODIFIED_BY] = 'admin'
+                record_map[UUID] = uuid4()
+                print(record_map, "recordddddddddd")
+                for key,value in record_map.items():
+                    setattr(data,key,value)
+                data.save()
+                return Response({STATUS: SUCCESS, DATA: "Data Succesfully Edited"}, status=status.HTTP_200_OK)
+        except Exception as ex:
+            print(ex, "exxxxxxxxxxxxxxx")
+            return Response({STATUS: ERROR, DATA: "Something went wrong"}, status=status.HTTP_400_BAD_REQUEST)
+
 
     def delete(self,request,uuid = None):
         if not uuid:
@@ -545,7 +659,7 @@ class SupplierDashboard_earningGraphView(APIView):
                     week_list[past.strftime("%A")] = final
                 data = getattr(models,COURSE_ENROLL_TABLE).objects.filter(**{"supplier_email":supplier_email,"created_date_time__week":week}).values_list("payment_detail__amount", flat=True)
                 total_earning = sum(list(data))
-                return Response({STATUS: SUCCESS,"total_earning": total_earning, "weekly_earning":week_list}, status=status.HTTP_200_OK)
+                return Response({STATUS: SUCCESS,"total_earning": total_earning, "data":week_list}, status=status.HTTP_200_OK)
             except Exception as ex: 
                 return Response({STATUS: ERROR, DATA: "Error in getting data"}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -569,7 +683,7 @@ class SupplierDashboard_earningGraphView(APIView):
 
                 total_earning = int(sum(list(data)))
                 return Response({STATUS: SUCCESS,
-                "total_earning": total_earning, "monthly_earning":month_list}, status=status.HTTP_200_OK)
+                "total_earning": total_earning, "data":month_list}, status=status.HTTP_200_OK)
             except Exception as ex:
                 return Response({STATUS: ERROR, DATA: "Error in getting data"}, status=status.HTTP_400_BAD_REQUEST)
         
@@ -601,7 +715,7 @@ class SupplierDashboard_earningGraphView(APIView):
                     data1 = getattr(models,COURSE_ENROLL_TABLE).objects.filter(**{"supplier_email":supplier_email,"created_date_time__year":month_List[i].split()[1], "created_date_time__month":strptime(month_List[i].split()[0],'%B').tm_mon}).values_list("payment_detail__amount", flat=True)
                     final_data[month_List[i].split()[0]] = float(sum(list(data1)))
                 return Response({STATUS: SUCCESS,
-                "total_earning": total_earning, "final_list":final_data}, status=status.HTTP_200_OK)
+                "total_earning": total_earning, "data":final_data}, status=status.HTTP_200_OK)
             except Exception as ex:
                 return Response({STATUS: ERROR, DATA: "Error in getting data"}, status=status.HTTP_400_BAD_REQUEST)
 
