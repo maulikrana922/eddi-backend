d8ca4ce4170d87634ab2389fc00566156a00f3e9
jap.d <jap.d@latitudetechnolabs.com>
2022-08-10 17:41:16 +0530

    course detail updated

--- eddi_app/supplier_views.py
+++ eddi_app/supplier_views.py
@@ -1541,7 +1541,7 @@ class SupplierOrganizationProfileview(APIView):
                         # data = getattr(models,USERSIGNUP_TABLE).objects.get(**{EMAIL_ID:email_id})
                         html_path = "supplier_organization_approved.html"
                         fullname = f'{data1.usersignup.first_name} {data1.usersignup.last_name}'
-                        context_data = {'fullname':fullname, "email_id":email_id}
+                        context_data = {'fullname':fullname, "email_id":supplier_email}
                         email_html_template = get_template(html_path).render(context_data)
                         email_from = settings.EMAIL_HOST_USER
                         recipient_list = (data1.supplier_email,)
