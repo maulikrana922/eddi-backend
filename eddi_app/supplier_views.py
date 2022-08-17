293c5bd96d530d17c80e0b0a91acbea8145720db
jap.d <jap.d@latitudetechnolabs.com>
2022-08-17 16:29:09 +0530

    email temps updated

--- eddi_app/supplier_views.py
+++ eddi_app/supplier_views.py
@@ -613,10 +613,10 @@ class GetCourseDetails(APIView):
                     course_enrolled = getattr(models,USER_PAYMENT_DETAIL).objects.filter(**{EMAIL_ID:email_id}).values_list("course__course_name", flat=True)
                     print(course_enrolled, "enrolleddd")
 
-                    # if user_subcategory:
-                    #     user_profile_interest = getattr(models,COURSEDETAILS_TABLE).objects.filter(**{STATUS_ID:1, IS_APPROVED_ID:1, IS_DELETED:False}).filter(Q(course_subcategory__subcategory_name__in=user_areaofinterest + user_subcategory) | Q(course_name__in=user_areaofinterest)).exclude(course_name__in = course_enrolled).order_by("-created_date_time")
-                    # else:
-                    user_profile_interest = getattr(models,COURSEDETAILS_TABLE).objects.filter(**{STATUS_ID:1, IS_APPROVED_ID:1, IS_DELETED:False}).filter(Q(course_subcategory__subcategory_name__in=user_areaofinterest + user_subcategory) | Q(course_category__category_name__in=user_areaofinterest + user_category + user_only_category)  | Q(course_name__in=user_areaofinterest)).exclude(course_name__in = course_enrolled).order_by("-created_date_time")
+                    if user_subcategory:
+                        user_profile_interest = getattr(models,COURSEDETAILS_TABLE).objects.filter(**{STATUS_ID:1, IS_APPROVED_ID:1, IS_DELETED:False}).filter(Q(course_subcategory__subcategory_name__in=user_areaofinterest + user_subcategory) | Q(course_name__in=user_areaofinterest)).exclude(course_name__in = course_enrolled).order_by("-created_date_time")
+                    else:
+                        user_profile_interest = getattr(models,COURSEDETAILS_TABLE).objects.filter(**{STATUS_ID:1, IS_APPROVED_ID:1, IS_DELETED:False}).filter(Q(course_subcategory__subcategory_name__in=user_areaofinterest + user_subcategory) | Q(course_category__category_name__in=user_areaofinterest + user_category + user_only_category)  | Q(course_name__in=user_areaofinterest)).exclude(course_name__in = course_enrolled).order_by("-created_date_time")
 
                     print(user_profile_interest, "intererer")
                     user_interest_course = user_profile_interest.values_list("course_name")
@@ -743,7 +743,7 @@ class GetCourseDetails(APIView):
                                 record_map[STATUS_ID] = 2
                                 try:
                                     html_path = INACTIVE_COURSE
-                                    context_data = {"course_name":request.POST.get(COURSE_NAME,data.course_name)}
+                                    context_data = {"fullname":str(data.supplier.first_name)+str(data.supplier.last_name),"course_name":request.POST.get(COURSE_NAME,data.course_name)}
                                     email_html_template = get_template(html_path).render(context_data)
                                     email_from = settings.EMAIL_HOST_USER
                                     recipient_list = (data.supplier.email_id,)
