8f1f6e8fb99a293d3215185005cf92bd9f53d1ed
Dream host <ravinder.s@latitudetechnolabs.com>
2022-10-17 12:03:12 +0000

    Update settings.py

--- eddi_backend/settings.py
+++ eddi_backend/settings.py
@@ -389,9 +389,10 @@ if env('DJANGO_ENV') == 'production':
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
 
-    DEFAULT_FILE_STORAGE = 'eddi_backend.storage_backend.MediaStorage'
-    STATICFILES_STORAGE = 'eddi_backend.s3boto3.S3StaticStorage'
-    STATICFILES_STORAGE = 'eddi_backend.s3boto3.S3ManifestStaticStorage'
+   # DEFAULT_FILE_STORAGE = 'eddi_backend.storage_backend.MediaStorage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3ManifestStaticStorage'
 
 else:
     STATIC_URL = '/static/'
