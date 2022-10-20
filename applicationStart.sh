07c332241f07afefca70521f0c4e422a9a6fef80
Dream host <ravinder.s@latitudetechnolabs.com>
2022-10-20 19:43:04 +0000

    Update applicationStart.sh

--- applicationStart.sh
+++ applicationStart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
-cd /var/www/html/eddi-backend/
-/usr/local/bin/pip3 install -r requirements.txt
+#cd /var/www/html/eddi-backend/
+#/usr/local/bin/pip3 install -r requirements.txt
 
 
 systemctl restart apache2
