20d0b2d5c4e4a941ecc6454f51ad2c9c8761ae2f
Dream host <ravinder.s@latitudetechnolabs.com>
2022-10-21 04:15:21 +0000

    Update afterInstall.sh

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 apt install software-properties-common -y
 add-apt-repository ppa:deadsnakes/ppa  -y
-
+apt-get update -y
 apt-get install python3.9  -y
 apt-get install python3.9-dev -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 apt-get install python3.9-distutils -y
-python3.9 get-pip.py
+/usr/bin/python3.9 get-pip.py
 echo 'export PATH=~/.local/bin/:$PATH' >> ~/.bashrc
 source ~/.bashrc
 apt-get install gcc -y
