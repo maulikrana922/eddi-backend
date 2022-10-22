d1bc610ebf3e14726f34ec0b7b61d4a374b4f440
Dream host <ravinder.s@latitudetechnolabs.com>
2022-10-22 14:17:22 +0000

    Update afterInstall.sh

--- afterInstall.sh
+++ afterInstall.sh
@@ -2,6 +2,8 @@
 python3.9 -V
 pip3 -V
 pip3 install -r "/var/www/html/eddi-backend/requirements.txt"
+apt-get update
+apt-get install acl
 sudo setfacl -R -m u:www-data:rwx "/var/www/html/eddi-backend"
 sudo setfacl -R -m g:www-data:rwx "/var/www/html/eddi-backend"
 sudo setfacl -R -m u:ubuntu:rwx "/var/www/html/eddi-backend"
